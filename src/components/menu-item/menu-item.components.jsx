05150d769cfd246572a1f5c20c2bd073c63fc8a9
Timothy John Manas <timothy.romero.manas@gmail.com>
2019-09-28 17:53:44 -0400

    Added Styling for image hover

--- src/components/menu-item/menu-item.components.jsx
+++ src/components/menu-item/menu-item.components.jsx
@@ -1,20 +1,28 @@
 import React from "react";
-import './menu-item.styles.scss'
+import "./menu-item.styles.scss";
 
 // The menu item is the single block within the Directory
-// We will pass in style for the background Image 
-const MenuItem = ({title,imageUrl, size}) => (
-  <div 
-    style={{
-      backgroundImage: `url(${imageUrl})`
-    }}
-  className={`${size} menu-item`}>
-
+// We will pass in style for the background Image
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`${size} menu-item`}>
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
     <div className="content">
-      <h1 className="title">{title}</h1>
+      <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
 );
 
-export default MenuItem
+export default MenuItem;
+
+/**
+
+- We moved the styling on the containing div "" onto the child div is because we want the image 
+to get bigger within the contrains of our containing div when we hover over it 
+
+ */
