f01ce4d71e94ef2a19d32436a9aebef31e388d74
Timothy John Manas <timothy.romero.manas@gmail.com>
2019-09-29 13:17:27 -0400

    Added header as component

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'
-
-
+import Header from './components/header/header.component'
 
 // The switch element tag here acts like a switch statement
 // It will only route to one  destination while ignoring other routes
+// We need to place the header OUTSIDE of the switch component. Why ? So our header is available for all pages
+
 function App() {
   // return <div>Hello World</div>;
   return (
     <div>
+      <Header /> 
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
