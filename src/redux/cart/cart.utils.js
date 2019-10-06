990e7527a3dfccbea3fdf9c7cc9bd59cd12b47aa
Timothy John Manas <timothy.romero.manas@gmail.com>
2019-10-06 19:50:51 -0400

    Created Add/Remove < & > buttons on Checkout page

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -15,3 +15,19 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     return [...cartItems, {...cartItemToAdd, quantity: 1}]
 
 }
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  )
+
+  // If Last Item on the list, then we use filter to get rid of entire product
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+  }
+
+  return cartItems.map(cartItem => 
+    cartItem.id === cartItemToRemove.id ? {...cartItem, quantity: cartItem.quantity - 1} : cartItem
+  )
+}
