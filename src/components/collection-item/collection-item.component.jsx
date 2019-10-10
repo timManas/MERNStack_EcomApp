d8a43fc2fd68bd82fecb9eeebf04c13e0b020b48
Timothy John Manas <timothy.romero.manas@gmail.com>
2019-10-10 19:15:21 -0400

    Added last min changes

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,25 +1,25 @@
-import React from "react";
-import { connect } from "react-redux";
+import React from 'react';
+import { connect } from 'react-redux';
 
-import CustomButton from "../custom-button/custom-button.component";
-import { addItem } from "../../redux/cart/cart.actions";
+import CustomButton from '../custom-button/custom-button.component';
+import { addItem } from '../../redux/cart/cart.actions';
 
-import "./collection-item.styles.scss";
+import './collection-item.styles.scss';
 
 const CollectionItem = ({ item, addItem }) => {
   const { name, price, imageUrl } = item;
 
   return (
-    <div className="collection-item">
+    <div className='collection-item'>
       <div
-        className="image"
+        className='image'
         style={{
           backgroundImage: `url(${imageUrl})`
         }}
       />
-      <div className="collection-footer">
-        <span className="name">{name}</span>
-        <span className="price">{price}</span>
+      <div className='collection-footer'>
+        <span className='name'>{name}</span>
+        <span className='price'>{price}</span>
       </div>
       <CustomButton onClick={() => addItem(item)} inverted>
         Add to cart
@@ -28,11 +28,11 @@ const CollectionItem = ({ item, addItem }) => {
   );
 };
 
-// Creating this prop called addItem  that will go into CollectionItem above
-// Wwhenever we dispatch, it will pass in addItem which gives us an object where the type is addObjet with payload
-// and dispatch it into our store and go through redux flow
 const mapDispatchToProps = dispatch => ({
   addItem: item => dispatch(addItem(item))
 });
 
-export default connect(null, mapDispatchToProps)(CollectionItem);
+export default connect(
+  null,
+  mapDispatchToProps
+)(CollectionItem);
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,25 +1,20 @@
 import CartActionTypes from './cart.types';
 
 export const toggleCartHidden = () => ({
-    type: CartActionTypes.TOGGLE_CART_HIDDEN
-})
+  type: CartActionTypes.TOGGLE_CART_HIDDEN
+});
 
-// Remember that Actions can have a type value and a payload value
-// Payload can be anything we want it to be
 export const addItem = item => ({
-    type: CartActionTypes.ADD_ITEM,
-    payload: item
-
-})
-
-export const clearItemFromCart = item => ({
-    type: CartActionTypes.CLEAR_ITEM_FROM_CART,
-    payload: item
-})
+  type: CartActionTypes.ADD_ITEM,
+  payload: item
+});
 
 export const removeItem = item => ({
-    type: CartActionTypes.REMOVE_ITEM,
-    payload: item
-})
+  type: CartActionTypes.REMOVE_ITEM,
+  payload: item
+});
 
-
+export const clearItemFromCart = item => ({
+  type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+  payload: item
+});
